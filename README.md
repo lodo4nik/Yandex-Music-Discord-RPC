b6360978fab9eb7e660b5c9582d2e2d24e2df132
lodo4nik <rompetaalex@gmail.com>
2023-05-07 13:30:19 +0300

    main commint

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# Yandex-Music-Discord-RPC
- Discord RPC for Yandex Music
+# Yandex Music Discord RPC
+
+[![N|Solid](https://i.imgur.com/sDQBibw.png)](https://i.imgur.com/sDQBibw.png)
+
+Yandex Music Discord RPC - простой RPC для Discord, показывающий, что вы слушаете на данный момент в Yandex Music (Яндекс.Музыка). 
+
+## Функции
+
+- Показывает название трека, исполнителя
+- Показывает обложку сингла/альбома
+- При наведении на логотип Yandex Music - показывает длительность трека
+- Кнопка "Слушать" переводит Вас на страницу с треком
+
+## Установка
+
+- `git clone https://github.com/lodo4nik/Yandex-Music-Discord-RPC`
+    >Замечание: Если отсутствует git, скачайте архив
+- `cd Yandex-Music-Discord-RPC`
+- `pip install -r requirements.txt`
+- Указываем acces_token в файл `config.py`
+- `python3 main.py
+
+## Важно
+- У Вас должен быть установлен Discord
+
+## FAQ
+> **Где получить acces_token?** 
+Об этом можно узнать в документации библиотеки [Yandex Music API](https://yandex-music.readthedocs.io/en/main/token.html)
+
+>**С какой задержкой обновляется трек?**
+По умолчанию - 3 секунды. Указать свою задержку можно в `main.py`
+
+>**Остались вопросы?**
+Напишите в Discord: `лодка#4433`
+
+## Лицензия
+**Yandex Music Discord RPC** распространяется под лицензией GPL-3.0. Подробнее в файле [LICENSE](https://github.com/lodo4nik/Yandex-Music-Discord-RPC/blob/main/LICENSE)
+**Yandex Music API** - LGPL-3
+**Яндекс.Музыка** - [Ссылка](https://yandex.ru/legal/music_mobile_agreement/)
+
+>**Оригинальный код написан NeyQ и распространяется под лицензией Apache**. Ссылка на репозиторий: https://github.com/NeynQ/Discord-YM-RPC
---
+++ main.py
@@ -0,0 +1,52 @@
+from pypresence import Presence
+from yandex_music import Client
+from yandex_music.exceptions import UnauthorizedError
+import time
+
+from config import access_token
+
+DELAY = 3
+
+def getTrack():
+    client = Client(access_token).init()
+    queues = client.queues_list()
+    last_queue = client.queue(queues[0].id)
+    last_track_id = last_queue.get_current_track()
+    last_track = last_track_id.fetch_track()
+    tid = last_track_id.track_id
+    url = f"https://music.yandex.ru/track/{tid}"
+    artists = ', '.join(last_track.artists_name())
+    title = last_track.title
+    image_link=last_track.get_cover_url(size="200x200")
+    duration_min = str((last_track.duration_ms // (1000 * 60)) % 60)
+    duration_sec = str((last_track.duration_ms // 1000) % 60)
+    return artists, title, url, image_link, duration_min, duration_sec
+
+client_id = '1079901231817965598'
+RPC = Presence(client_id)
+RPC.connect() # mq
+
+while True:
+    try:
+        track = getTrack()
+        RPC.update(
+            buttons=[{"label": "Слушать", "url": track[2]}],
+            state="by " + track[0],
+            details="" + track[1],
+            large_image=track[3],
+            large_text=track[1],
+            small_image="https://free-png.ru/wp-content/uploads/2021/12/free-png.ru-60.png",
+            small_text=track[4] + ":" + track[5]
+        )
+    except (UnauthorizedError, UnicodeEncodeError):
+        print("Неверный access_token!")
+        print("Введите верный токен в файле config.py!")
+        break
+    except:
+        RPC.update(
+            state="Yandex Music RPC by lodo4nik",
+            large_image="https://free-png.ru/wp-content/uploads/2021/12/free-png.ru-60.png",
+            large_text="Yandex Music",
+            buttons=[{"label": "Repository", "url": "https://github.com/lodo4nik"}],
+        )
+    time.sleep(DELAY)
